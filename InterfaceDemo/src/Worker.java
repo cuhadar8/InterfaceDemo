1e77c8e8ca0240fc918a8277cf6e229d1a1d5464
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-07 19:16:25 +0300

    InterfaceDemo

---
+++ InterfaceDemo/src/Worker.java
@@ -0,0 +1,22 @@
+//bizim çalışanımız
+public class Worker implements IWorkable, IEatable, IPayable {
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
